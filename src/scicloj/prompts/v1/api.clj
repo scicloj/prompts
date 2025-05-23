8b69ce190e715e1608425343d9aff1c6949fe6a3
daslu <daniel.slutsky@gmail.com>
2025-05-24 02:53:26 +0300

    simplified API

--- src/scicloj/prompts/v1/api.clj
+++ src/scicloj/prompts/v1/api.clj
@@ -1,43 +1,23 @@
 (ns scicloj.prompts.v1.api
   (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.string :as str]))
 
-(defn- load-catalog
-  "Load the prompts catalog from resources"
-  []
-  (-> "prompts-catalog.edn"
-      io/resource
-      slurp
-      edn/read-string))
+(defn generate-prompt
+  "Generate a prompt based on the tasks described in the tags."
+  [desired-tags]
+  (->> "prompts-catalog.edn"
+       io/resource
+       slurp
+       edn/read-string
+       :prompts
+       (filter (fn [[_ {:keys [tags]}]]
+                 (some #(tags %) desired-tags)))
+       (map (fn [[prompt-name _]]
+              (->> prompt-name
+                   (str "prompts/")
+                   io/resource
+                   slurp)))
+       (str/join "\n\n")
+       (str (slurp "CLAUDE-base.md") "\n\n")))
 
-(defn list-prompts
-  "List all available prompts"
-  []
-  (:prompts (load-catalog)))
-
-(defn get-prompt-metadata
-  "Get metadata for a specific prompt"
-  [prompt-id]
-  (get-in (load-catalog) [:prompts prompt-id]))
-
-(defn get-prompt
-  "Get the content of a specific prompt"
-  [prompt-id]
-  (when-let [metadata (get-prompt-metadata prompt-id)]
-    (-> (str "prompts/" prompt-id)
-        io/resource
-        slurp)))
-
-(defn find-prompts-by-tag
-  "Find prompts that have a specific tag"
-  [tag]
-  (->> (list-prompts)
-       (filter (fn [[_ metadata]] (contains? (:tags metadata) tag)))
-       (into {})))
-
-(defn get-all-tags
-  "Get all unique tags from all prompts"
-  []
-  (->> (list-prompts)
-       (mapcat (fn [[_ metadata]] (:tags metadata)))
-       (into #{})))
